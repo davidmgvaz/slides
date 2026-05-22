95ae5b154aef7fe36c51b01139aa2af5a42d9389
David Vaz <david.vaz@boostinc.com>
2026-05-22 14:01:40 +0100

    isla improvements

--- isla-bd-django52/global-bottom.vue
+++ isla-bd-django52/global-bottom.vue
@@ -3,7 +3,12 @@ import logo from './isla-logo.png'
 </script>
 
 <template>
-  <div class="isla-corner-logo"><img :src="logo" alt="ISLA" /></div>
+  <div
+    class="isla-corner-logo"
+    v-if="$slidev.nav.currentPage > 1 && $slidev.nav.currentPage < $slidev.nav.total"
+  >
+    <img :src="logo" alt="ISLA" />
+  </div>
   <footer class="isla-foot" v-if="$slidev.nav.currentPage > 1">
     <span class="fl">Django &middot; Databases &middot; LICE</span>
     <span class="fr">{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</span>
